885cdf7ef21a5845877d9c47328dd94b65ce773f
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-03-30 13:35:44 +0900

    [Silver III] Title: 등수 매기기, Time: 464 ms, Memory: 52640 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+expected = []
+for _ in range(n):
+    expected.append(int(input()))
+
+expected.sort()
+
+result = 0
+for i in range(1, n+1):
+    result += abs(i-expected[i-1])
+print(result)
