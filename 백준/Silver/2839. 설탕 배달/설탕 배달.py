4502fa2cd6697de0324cdcb21ce918502d6e1346
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-13 22:41:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 276 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -4,8 +4,8 @@ input = sys.stdin.readline
 N = int(input())
 list = []
 
-for i in range(0, 2000):
-    for j in range(0, 2000):
+for i in range(0, (N//3)+1):
+    for j in range(0, (N//5)+1):
         if 5*j+3*i == N:
             list.append(j+i)
 
@@ -13,3 +13,4 @@ if len(list) > 0:
     print(min(list))
 else:
     print(-1)
+
