9e566ae74e507759a650d36203bd64d9469d7524
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-13 22:53:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,16 +1,15 @@
 import sys
 input = sys.stdin.readline
+N=int(input())
+cnt=0
+while N>=0:
+    if N%5==0:
+        cnt+=N//5
+        print(cnt)
+        break
+    N-=3
+    cnt+=1
 
-N = int(input())
-list = []
-
-for i in range(0, (N//3)+1):
-    for j in range(0, (N//5)+1):
-        if 5*j+3*i == N:
-            list.append(j+i)
-
-if len(list) > 0:
-    print(min(list))
 else:
     print(-1)
-
+        
