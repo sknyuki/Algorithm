0e375a061b32bc1b1d6f12afdc8506bfa761b543
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-16 00:51:30 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -7,10 +7,6 @@ change = reversed(list(int(input()) for _ in range(N)))
 cnt = 0
 for i in change:
     if M >= i:
-        if M == 1:
-            cnt+=1
-            break
-        else:
             temp, C = divmod(M, i)
             cnt += temp
             M = C
