c05cd8f9038cf4b5ba776440280e19f653ac993e
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-16 07:03:41 +0900

    [Silver III] Title: 문자열 집합, Time: 120 ms, Memory: 42368 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+listA = set(list(input().strip() for _ in range(N)))
+listB = list(input().strip() for _ in range(M))
+count = 0
+for i in listB:
+    if i in listA:
+        count += 1
+print(count)
