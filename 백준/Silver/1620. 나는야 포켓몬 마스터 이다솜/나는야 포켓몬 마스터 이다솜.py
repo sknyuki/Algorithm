05e1465f99aaebcf963f6eacfc031580b0d828a7
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-19 06:47:22 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 248 ms, Memory: 58936 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -3,12 +3,14 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 poke = list(input().strip() for _ in range(N))
+poke_num = {poke[i]: i for i in range(len(poke))}
+
+poke_id = {i: poke[i] for i in range(len(poke))}
 
 for i in range(M):
     A = input().strip()
-    try:
-        A = int(A)
-        print(poke[A-1])
+    if A.isdigit():
+        print(poke_id[int(A)-1])
 
-    except:
-        print(poke.index(A)+1)
+    else:
+        print(poke_num[A]+1)
