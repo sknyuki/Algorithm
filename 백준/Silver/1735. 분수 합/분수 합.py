54d6b4a3cf0a80747a1c9e9422844fff4565b4dd
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-21 21:58:06 +0900

    [Silver III] Title: 분수 합, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.py
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -2,23 +2,19 @@ import sys
 input = sys.stdin.readline
 
 
-def Greatest_common_divisor(x, y):
+def GCD(x, y):
     if y == 0:
         return x
     else:
-        return Greatest_common_divisor(y, x % y)
-
-
-def Least_common_multiple(x, y):
-    return (x*y)//Greatest_common_divisor(x, y)
+        return GCD(y, x % y)
 
 
 A, B = map(int, input().split())
 C, D = map(int, input().split())
 
 
-F = Least_common_multiple(max(B, D), min(B, D))
-E = A*(F//B)+C*(F//D)
-temp = Greatest_common_divisor(max(F, E), min(F, E))
+E = A*D+B*C
+F = B*D
+temp = GCD(max(F, E), min(F, E))
 print(E//temp,
       F//temp)
