122129b1c484167f4a9bda96474be311ae331044
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-22 23:39:50 +0900

    [Silver II] Title: 베르트랑 공준, Time: 164 ms, Memory: 35172 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -3,25 +3,41 @@ input = sys.stdin.readline
 
 
 def solution(N):
-    if N == 1:
-        return False
+    pn = [True]*(N+1)
+    pn[0] = pn[1] = False
     for i in range(2, int(N**0.5)+1):
-        if N % i == 0:
-            return False
-    return True
+        if pn[i]:
+            for j in range(i*i, N+1, i):
+                pn[j] = False
+    primes = list(i for i in range(len(pn)) if pn[i])
+    return primes
 
 
 def find_pn(N, M):
+    answer = 0
     cnt = 0
-    ls = []
-    for i in range(N+1, M+1):
-        if solution(i):
-            cnt += 1
-    return cnt
+    temp = 0
+    while True:
+        temp = primes_list[cnt]
+        if N < temp <= M:
+            answer += 1
+        elif temp > M:
+            break
+
+        cnt += 1
+    return answer
 
 
+input_list = []
 while True:
     N = int(input())
     if N == 0:
         break
-    print(find_pn(N, 2*N))
+    input_list.append(N)
+
+primes_list = solution(max(input_list)*3)
+for i in input_list:
+    if i == 1:
+        print(1)
+    else:
+        print(find_pn(i, 2*i))
