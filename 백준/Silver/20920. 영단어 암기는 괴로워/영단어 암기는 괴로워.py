4a79f5f7617b50d2625a41cacbe39b121e9b22d2
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-27 21:17:53 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 332 ms, Memory: 60908 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -7,7 +7,7 @@ sentence = [i for _ in range(A) if len(i := input().strip()) >= B]
 
 counter_sectence = collections.Counter(sentence)
 sentence = sorted(counter_sectence.items(),
-                  key=lambda X: (-X[1], -len(X[0]), X[0]))
+                  key=lambda X: (-X[1], -len(X[0]),X[0]))
 
 for i in sentence:
     print(i[0])
