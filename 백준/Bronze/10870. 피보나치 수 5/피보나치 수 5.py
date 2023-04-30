f6832fe73e58cc8f83b1d7b80fd50297363eda2c
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-04-30 16:12:30 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,11 +1,12 @@
 import sys
 input = sys.stdin.readline
-N=int(input())
-listA = []
-for i in range(N+1):
-    if i < 2:
-        listA.append(i)
-    else:
-        listA.append(listA[i-2]+listA[i-1])
 
-print(listA[N])
+
+def fibo(N):
+    if N < 2:
+        return N
+    else:
+        return(fibo(N-2)+fibo(N-1))
+
+
+print(fibo(int(input())))
