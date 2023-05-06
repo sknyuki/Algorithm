47402a78fb28445cf740bc5f4066f4ba1fedd482
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-05-07 00:39:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 592 ms, Memory: 68916 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+def DFS(N):
+    global cnt
+    cnt += 1
+    visited[N] = True
+    result[N] = cnt
+    for i in graph[N]:
+        if not visited[i]:
+            DFS(i)
+
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for i in range(M):
+    A, B = map(int, input().split())
+    graph[A].append(B)
+    graph[B].append(A)
+for i in graph:
+    i.sort()
+
+visited = [False]*(N+1)
+result = [0]*(N+1)
+cnt = 0
+DFS(R)
+for i in range(1, len(result)):
+    print(result[i])
