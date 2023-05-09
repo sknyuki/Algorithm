99c07cafcfec9f6490e6ec642c90828e96ac6856
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-05-09 13:07:17 +0900

    [Silver II] Title: 유기농 배추, Time: 72 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -3,21 +3,23 @@ from collections import deque
 input = sys.stdin.readline
 T = int(input())
 
+#단지문제와 동일
 
-def bfs(x, y):
+def bfs(y, x):
     vector = [[0, 1], [1, 0], [0, -1], [-1, 0]]
     que = deque()
-    graph[x][y] = 0
+    graph[y][x] = 0
     cnt = 1
-    que.append([x, y])
-    while que:
-        x, y = que.popleft()
+    que.append([y, x])
+    while que:    #루틴을 돌며 인접한 배추의 수를 세어줌
+        y, x = que.popleft()
         for vy, vx in vector:
-            if 0 <= x+vx < N and 0 <= y+vy < M and graph[x+vx][y+vy] == 1:
-                graph[x+vx][y+vy] = 0
+            if 0 <= x+vx < M and 0 <= y+vy < N and graph[y+vy][x+vx] == 1:
+                graph[y+vy][x+vx] = 0
                 cnt += 1
-                que.append([x+vx, y+vy])
+                que.append([y+vy, x+vx])
     cnt_list.append(cnt)
+
 
 
 len_cnt_list = []
@@ -28,9 +30,9 @@ for i in range(T):
     graph = [[0]*M for i in range(N)]
     for j in range(K):
         A, B = map(int, input().split())
-        graph[B][A] = 1
-    for y in range(M):
-        for x in range(N):
-            if graph[x][y] == 1:
-                bfs(x, y)
+        graph[B][A] = 1    #그래프작성
+    for x in range(M):
+        for y in range(N):
+            if graph[y][x] == 1:    #그래프 내부 1을 찾음 BFS의 시작점 탐색
+                bfs(y, x)
     print(len(cnt_list))
