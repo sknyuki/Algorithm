9e0f3772397c3b67dbbb6e91afe275103b28f06c
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-05-14 14:45:01 +0900

    [Bronze III] Title: 二人三脚 (Three-Legged Race), Time: 64 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Bronze/27326. 二人三脚 （Three－Legged Race）/二人三脚 （Three－Legged Race）.py
+++ 백준/Bronze/27326. 二人三脚 （Three－Legged Race）/二人三脚 （Three－Legged Race）.py
@@ -3,7 +3,7 @@ from collections import deque
 input = sys.stdin.readline
 N = int(input())
 listA = list(map(int, input().split()))
-counting = [0]*(max(listA)+1)
+counting = [0]*(N+1)
 for i in listA:
     counting[i] += 1
 print(counting.index(1))
