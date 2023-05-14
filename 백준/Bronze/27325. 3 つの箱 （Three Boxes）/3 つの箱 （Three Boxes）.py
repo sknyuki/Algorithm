063cbf2aedca927f466942189411d3fc4da548e7
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-05-14 14:33:32 +0900

    [Bronze III] Title: 3 つの箱 (Three Boxes), Time: 64 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Bronze/27325. 3 つの箱 （Three Boxes）/3 つの箱 （Three Boxes）.py
+++ 백준/Bronze/27325. 3 つの箱 （Three Boxes）/3 つの箱 （Three Boxes）.py
@@ -5,15 +5,13 @@ stack = 1
 cnt = 0
 N = int(input())
 S = input().strip()
-for i in range(len(S)):
-    if S[i] == "R":
-        stack += 1
-        if stack > 3:
-            stack = 3
+for i in S:
+    if i == "R":
+        if stack < 3:
+            stack += 1
         if stack == 3:
             cnt += 1
     else:
-        stack -= 1
-        if stack < 1:
-            stack = 1
+        if stack > 1:
+            stack -= 1
 print(cnt)
