64a76f35b8fef3b5eeb9e705e27b34bcca3d1c29
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-05-23 21:19:59 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -2,11 +2,13 @@ import sys
 input=sys.stdin.readline
 
 def fibo_dfs(n):
-    if n==1 or n==2:
-        return 1
-    else:
-        return(fibo_dfs(n-1)+fibo_dfs(n-2))
-
+    dp=[0]*(n+1)
+    dp[1],dp[2]=1,1
+    
+    for i in range(3,n+1):
+       
+        dp[i]=dp[i-1]+dp[i-2]
+    return dp[n]
 
 def fibo_DP(n):
     dp=[0]*(n+1)
