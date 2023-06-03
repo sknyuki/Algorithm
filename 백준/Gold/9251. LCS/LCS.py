48eede87bf0e60ed5bd67fd4a2eb28c4ef7b641c
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-06-03 23:22:00 +0900

    [Gold V] Title: LCS, Time: 512 ms, Memory: 55720 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,34 +1,33 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-#       A C A Y K P
-#     0 0 0 0 0 0 0
-#   C 0 0 1 1 1 1 1
-#   A 0 1 1 2 2 2 2
-#   P 0 1 1 2 2 2 3
-#   C 0 1 2 2 2 2 3 
-#   A 0 1 2 3 3 3 3
-#   K 0 1 2 3 3 4 4
+#    A C A Y K P
+#    0 0 0 0 0 0
+# C 0 0 1 1 1 1 1
+# A 0 1 1 2 2 2 2
+# P 0 1 1 2 2 2 3
+# C 0 1 2 2 2 2 3
+# A 0 1 2 3 3 3 3
+# K 0 1 2 3 3 4 4
 
-# solution
-# 1)세로열(B)하나씩 가로행(A)탐색 
-# 2)B[i]==A[j]이면 dp[i][j]=dp[i-1][j-1]+1
-# 3)아닐 경우 max(dp[y][x-1],dp[y-1][x])를 반영
+# 탐색중인 B의 값이 A의 값과 일치하면 dp[y][x]=dp[y-1][x]+1
+# else max(dp[y][x-1],dp[y-1][x])
 
+A = list(input().strip())
+B = list(input().strip())
+LA, LB = len(A)+1, len(B)+1
+dp = [[0]*(LA) for _ in range(LB)]
 
-A=list(input().strip())
-B=list(input().strip())
-AN=len(A)+1
-BN=len(B)+1
-
-dp=[[0]*AN for _ in range(BN)] #2차 배열 생성
-
-for y in range(1,BN):
-    for x in range(1,AN):
-        if A[x-1]==B[y-1]: #solution 과정 1
-            dp[y][x]=dp[y-1][x-1]+1 #solution 과정 2
+for y in range(1, LB):
+    for x in range(1, LA):
+        if A[x-1] == B[y-1]:
+            dp[y][x] = dp[y-1][x-1]+1
         else:
-            dp[y][x]=max(dp[y][x-1],dp[y-1][x]) #solution 과정 3
+            dp[y][x] = max(dp[y][x-1], dp[y-1][x])
 
-#print(*dp,sep="\n")
-print(dp[BN-1][AN-1])
+
+print(dp[LB-1][LA-1])
+
+
+
+
