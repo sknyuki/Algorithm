7c5ecfce90783fa4d919112e6a903f104b78f6c5
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-06-03 17:59:48 +0900

    [Gold V] Title: 평범한 배낭, Time: 5176 ms, Memory: 416612 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+
+#      1  2  3  4  5  6  7
+#3/6   0  0  6  6  6  6  6
+#4/8   0  0  6  8  8  8  14 ->max(8+6,6)
+#5/12  0  0  6  8  12 12 14
+#6/13  0  0  6  8  12 13 14 ->max(A[y][1]+dp[y-1][x-A[y][0]],dp[y-1][x])
+#                               A[y][1]=현재 물건가치
+#                    dp[y-1][x-A[y][0]]=이전물건중 남은 무게만큼에 할당되는 가치
+#                            dp[y-1][x]=이전물건중 지금 무게에 할당되는 가치
+
+N,K=map(int,input().split())
+A=sorted([list(map(int,input().split()))for _ in range(N)]+[[0,0]])
+dp=list([0]*(K+1) for _ in range(N+1))
+
+for y in range(1,N+1):
+    for x in range(1,K+1):
+        if A[y][0]>x:           #해당 물건이 더 큰 경우 dp[y-1][x]값 배치
+            dp[y][x]=dp[y-1][x]
+        else:                   #들어가는 경우
+            dp[y][x]=max(A[y][1]+dp[y-1][x-A[y][0]],dp[y-1][x])
+
+print(dp[N][K])
+            
