52408cd8fe326391b5e4197fcabbbfbd903c50e5
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-06-05 10:02:43 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,16 +1,17 @@
 import sys
 input = sys.stdin.readline
-# 규칙
-# 1,1,1,2,2,3,4,5,7,9 ->10 개 는 주어져있음
-# 11->12(3+9)
-# s[i-5]+s[i-1]
-# 12->16(4+12)
-# s[12-5]+s[12-1]
-T = int(input())
+
+#1, 1, 1, 2, 2, 3, 4, 5, 7, 9
+# 12->9+3
+N = int(input())
+A = [int(input()) for _ in range(N)]
+dp = [0]*N
 dp = [1, 1, 1, 2, 2, 3, 4, 5, 7, 9]+[0]*91
-list_n = [int(input()) for _ in range(T)]
-for i in range(max(list_n)):
-    if i > 9:
-        dp[i] = dp[i-1]+dp[i-5]
-for i in list_n:
+
+
+for j in range(5, max(A)):
+    dp[j] = dp[j-1]+dp[j-5]
+
+for i in A:
     print(dp[i-1])
+
