90476fb124fc008982ee53fd5e24ca86422a4291
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-06-15 05:52:55 +0900

    [Gold V] Title: 치킨 배달, Time: 172 ms, Memory: 115452 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,27 +1,28 @@
 import sys
 from itertools import combinations
-
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-city = list(list(map(int, input().split())) for _ in range(n))
+N, M = map(int, input().split())
+city = list(list(map(int, input().split())) for _ in range(N))
 result = 999999
 house = []      # 집의 좌표
 chick = []      # 치킨집의 좌표
 
-for i in range(n):
-    for j in range(n):
+for i in range(N):
+    for j in range(N):
         if city[i][j] == 1:
             house.append([i, j])
         elif city[i][j] == 2:
             chick.append([i, j])
-            
-for chi in combinations(chick, m):  # m개의 치킨집 선택
+
+# 백트레킹 없이 combination으로 구현 치킨집 M개까지 감소
+#print(list(combinations(chick, M)))
+for c in list(combinations(chick, M)):  # m개의 치킨집 좌표
     temp = 0            # 도시의 치킨 거리
-    for h in house: 
+    for h in house:
         chi_len = 999   # 각 집마다 치킨 거리
-        for j in range(m):
-            chi_len = min(chi_len, abs(h[0] - chi[j][0]) + abs(h[1] - chi[j][1]))
+        for j in range(M):
+            chi_len = min(chi_len, abs(h[0] - c[j][0]) + abs(h[1] - c[j][1]))
         temp += chi_len
     result = min(result, temp)
 
