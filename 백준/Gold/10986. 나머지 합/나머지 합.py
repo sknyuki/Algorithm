1e96e8ced84a26ba8b53a17ae8e34f8097fbd03a
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-06-23 15:54:33 +0900

    [Gold III] Title: 나머지 합, Time: 588 ms, Memory: 262740 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,33 +1,29 @@
 import sys
-from itertools import combinations
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-# 1 3 6 7 9         3 6 9
-#   2 5 6 8         6
-#     3 4 6         3 6
-#       1 3         3
-#         2
+#누적합의 집합을 만들고
+#카운팅(C)으로 M으로 나누었을때 같은 값을 가지는 집합을 만들어준다 
+#인덱스의 값이 2이상일때
+    #(M으로 나누었을때 같은 값을 가지는 값들이 2이상일때)
+#C[i]C2(조합)+C[0](M으로 나누어 떨어질때)를 return
 
-
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-answer = 0
-S = []  # 합배열
-C = [0]*M  # 카운팅
-mysum = 0
-
-for i in A:
-    mysum += i
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+S=[]
+mysum=0
+C=[0]*M #카운팅
+answer=0
+for i in A:#누적합을 구하고
+    mysum+=i
     S.append(mysum)
-# print(S)
 
-for i in S:
-    if i % M == 0:
-        answer += 1
-    C[i % M] += 1
-# print(Sm)
+for i in S: #나머지 집합을 만들고
+    if i%M==0:
+        answer+=1 #3으로 나누어 떨어질 때 +1
+    C[i%M]+=1 #else가 아닌 if 다음에 넣어줘야함
 
 for i in range(M):
-    if C[i] > 1:  # 같은 나머지값을 가지는 수가 2개 이상있으면
-        answer += (C[i]*(C[i]-1))//2
+    if C[i]>1:#2 이상이면 
+        answer+=(C[i]*(C[i]-1))//2 #c[i]C2
 print(answer)
+
