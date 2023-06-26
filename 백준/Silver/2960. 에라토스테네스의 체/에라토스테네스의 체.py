85e150dde93c13e33f2cb0c8c6b30bb6ebb0a859
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-06-26 23:34:07 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+tmp = 0
+sieve = [True] * (N + 1)
+for i in range(2, N + 1):
+    for j in range(i, N + 1, i):
+        if sieve[j] != False:
+            sieve[j] = False
+            tmp += 1
+            if tmp == K:
+                print(j)
