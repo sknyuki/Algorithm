69998d4cec872858f091fad65804f2d293db562c
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-07-03 17:44:54 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 72 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+def bfs(y, x):
+    q = deque()
+    q.append((y, x))
+    c = graph[y][x]
+    graph[y][x] = 1
+    cnt = 0
+    while q:
+        y, x = q.popleft()
+        cnt += 1
+        for dy, dx in vector:
+            Y, X = y+dy, x+dx
+            if (0 <= Y < M) and (0 <= X < N) and graph[Y][X] == c:
+                q.append((Y, X))
+                graph[Y][X] = 1
+    return cnt
+
+N, M = map(int, input().split())
+graph = [list(input()) for _ in range(M)]
+vector = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+w = b = 0
+for i in range(M):
+    for j in range(N):
+        if graph[i][j] == 'W':
+            w += bfs(i, j)**2
+        elif graph[i][j] == 'B':
+            b += bfs(i, j)**2
+print(w, b)
