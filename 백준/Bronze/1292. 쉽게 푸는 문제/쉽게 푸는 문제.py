884582e25e890c1631ff2e322650ad0227cf9ec6
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-07-07 22:57:21 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 100 ms, Memory: 36548 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+a, b = map(int, sys.stdin.readline().split())
+temp = []
+
+for i in range(1, b + 1):
+    for j in range(i):
+        temp.append(i)
+
+
+print(sum(temp[a - 1:b]))
