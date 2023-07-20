d6b238e4d1948de871af6efda1b0374ebed4318e
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-07-21 00:17:42 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 64 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,30 +1,47 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-listC = [input().strip() for _ in range(N)]
+# 스타트가 검은색인 경우와 흰색인 경우
+# 다음 색이 같은 색이면 +1인 형식
+# list에 K만큼 돌면 저장 해나가는 형식
+# 최종적으로 min값을 출력
 
-# 체스판의 크기를 8*8로 잘라야한다
-# 자른 형태로 이동을하게 됨
-count = []
+# [0,0,0,0,0]
+# [0,0,1,1,2]
+# [0,1,2,3,4]
+# [0,1,3,4,5]
+# [0,2,4,5,6]
 
-for i in range(N-7):
-    for j in range(M-7):
-        xdraw = 0  # W부터 시작이 되었을때
-        ydraw = 0  # B부터 시작이 되었을떄
 
-        for a in range(i, i+8):
-            for b in range(j, j+8):
-                if (a+b) % 2 == 0:
-                    if listC[a][b] != "W":
-                        xdraw += 1
-                    else:
-                        ydraw += 1
-                else:
-                    if listC[a][b] != "B":
-                        xdraw += 1
-                    else:
-                        ydraw += 1
-        count.append(min(xdraw, ydraw))
+N, M= map(int, input().split())
+K=8
+board = [list(input().strip()) for _ in range(N)]
+dp = [[0]*(M+1) for _ in range(N+1)]
+#print(*board, sep="\n")
 
-print(min(count))
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        if(i+j) % 2 == 0:
+            if board[i-1][j-1] == "B":
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]
+            else:
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+1
+        else:
+            if board[i-1][j-1] == "W":
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]
+            else:
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+1
+
+#print(*dp, sep="\n")
+# print("=============")
+max_value = -float('inf')
+min_value = float('inf')
+for r in range(K, N+1):
+    for c in range(K, M+1):
+        max_value = max(dp[r][c] - dp[r-K][c] - dp[r]
+                        [c-K] + dp[r-K][c-K], max_value)
+        min_value = min(dp[r][c] - dp[r-K][c] - dp[r]
+                        [c-K] + dp[r-K][c-K], min_value)
+
+print(min(min_value, max_value, K*K - min_value, K*K - max_value))
