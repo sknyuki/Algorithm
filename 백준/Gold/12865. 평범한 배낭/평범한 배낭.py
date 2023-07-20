e0ffc74c8f7e45766b3c2cb02019d758b9776b84
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-07-20 18:33:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 5428 ms, Memory: 279396 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,25 +1,26 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-#      1  2  3  4  5  6  7
-#3/6   0  0  6  6  6  6  6
-#4/8   0  0  6  8  8  8  14 ->max(8+6,6)
-#5/12  0  0  6  8  12 12 14
-#6/13  0  0  6  8  12 13 14 ->max(A[y][1]+dp[y-1][x-A[y][0]],dp[y-1][x])
-#                               A[y][1]=현재 물건가치
-#                    dp[y-1][x-A[y][0]]=이전물건중 남은 무게만큼에 할당되는 가치
-#                            dp[y-1][x]=이전물건중 지금 무게에 할당되는 가치
+#     0  1  2  3  4  5  6  7
+#     0  0  0  0  0  0  0  0
+# 6/13 0  0  0  0  0  0  13 13
+# 4/8  0  0  0  0  8  8  13 13
+# 3/6  0  0  0  6  8  8  13 14
+# 5/12 0  0  0  6  8  12 13 14
+# dp[y][x]=dp[y-1][x-A[y-1][0]]+A[y-1][1]
 
-N,K=map(int,input().split())
-A=sorted([list(map(int,input().split()))for _ in range(N)]+[[0,0]])
-dp=list([0]*(K+1) for _ in range(N+1))
+N, K = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0]*(K+1) for _ in range(N+1)]
 
-for y in range(1,N+1):
-    for x in range(1,K+1):
-        if A[y][0]>x:           #해당 물건이 더 큰 경우 dp[y-1][x]값 배치
-            dp[y][x]=dp[y-1][x]
-        else:                   #들어가는 경우
-            dp[y][x]=max(A[y][1]+dp[y-1][x-A[y][0]],dp[y-1][x])
 
-print(dp[N][K])
-            
+for y in range(1, N+1):
+    for x in range(1, K+1):
+        if x >= A[y-1][0]:  # 들어 갈 수 있을때
+            dp[y][x] = max(dp[y-1][x-A[y-1][0]]+A[y-1][1],
+                           dp[y-1][x])  # 원래 가치+ 새로운 가치
+        else:  # 안 들어 갈때
+            dp[y][x] = dp[y-1][x]
+
+print(dp[-1][-1])
+#print(*dp, sep="\n")
