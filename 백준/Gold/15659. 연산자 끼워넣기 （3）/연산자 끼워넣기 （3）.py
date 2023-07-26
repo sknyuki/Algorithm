9bd8523a6d8e2590434fc46b263817d810b389af
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-07-27 00:35:53 +0900

    [Gold IV] Title: 연산자 끼워넣기 (3), Time: 816 ms, Memory: 124592 KB -BaekjoonHub

---
+++ 백준/Gold/15659. 연산자 끼워넣기 （3）/연산자 끼워넣기 （3）.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+
+def dfs(expression, depth, use_cnt):
+    global max_result, min_result
+    if depth == N:
+        tmp = eval(expression)#연산 우선순위 알아서 처리되도록 eval로 처리
+        max_result = max(max_result, tmp)
+        min_result = min(min_result, tmp)
+        return
+    
+    if use_cnt[0] < p_cnt:
+        use_cnt[0] += 1
+        dfs(expression + "+" + str(nums[depth]), depth + 1, use_cnt)
+        use_cnt[0] -= 1
+        
+    if use_cnt[1] < mi_cnt:
+        use_cnt[1] += 1
+        dfs(expression + "-" + str(nums[depth]), depth + 1, use_cnt)
+        use_cnt[1] -= 1
+
+    if use_cnt[2] < mu_cnt:
+        use_cnt[2] += 1
+        dfs(expression + "*" + str(nums[depth]), depth + 1, use_cnt)
+        use_cnt[2] -= 1
+
+    if use_cnt[3] < d_cnt:
+        use_cnt[3] += 1
+        dfs(expression + "//" + str(nums[depth]), depth + 1, use_cnt)
+        use_cnt[3] -= 1
+
+
+N = int(input())
+nums = list(map(int, input().split()))
+p_cnt, mi_cnt, mu_cnt, d_cnt = map(int, input().split())
+max_result =-float('inf')
+min_result = float('inf')
+dfs(str(nums[0]), 1, [0, 0, 0, 0])
+print(max_result)
+print(min_result)
