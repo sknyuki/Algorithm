d20ca6ec39818bb911211b5fecb8fe70f7f17513
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-09-07 01:35:33 +0900

    [Bronze II] Title: 막대기, Time: 100 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+t = int(input())
+stack = []
+
+for _ in range(t):
+    bar = int(input())
+
+    while stack and stack[-1] <= bar:
+        stack.pop()
+    stack.append(bar)
+
+print(len(stack))
