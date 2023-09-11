6240708dcd703c3d18bd5f1c0df2b5cefccab2a1
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-09-12 07:28:22 +0900

    [Silver IV] Title: 에르다 노바와 오리진 스킬, Time: 1496 ms, Memory: 201628 KB -BaekjoonHub

---
+++ 백준/Silver/29791. 에르다 노바와 오리진 스킬/에르다 노바와 오리진 스킬.py
@@ -0,0 +1,28 @@
+def count_kills(n, m, d, s):
+    s.sort()
+    d.sort()
+    E_before = d[0]
+    O_before = s[0]
+    E_kills = 1
+    O_kills = 1
+
+    if len(d) > 1:
+        for i in range(1, n):
+            if d[i]-E_before >= 100:
+                E_kills += 1
+                E_before = d[i]
+    if len(s) > 1:
+        for j in range(1, m):
+            if s[j]-O_before >= 360:
+                O_kills += 1
+                O_before = s[j]
+
+    return [E_kills, O_kills]
+
+
+n, m = map(int, input().split())
+d = list(map(int, input().split()))
+s = list(map(int, input().split()))
+
+result = count_kills(n, m, d, s)
+print(*result)
