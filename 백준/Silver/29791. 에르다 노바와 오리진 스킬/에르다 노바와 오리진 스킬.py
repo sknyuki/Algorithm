bb31f54454f7252e1b7b36aeb1b10180958e0c6d
sknyuki <96415820+sknyuki@users.noreply.github.com>
2023-09-12 07:55:30 +0900

    [Silver IV] Title: 에르다 노바와 오리진 스킬, Time: 1408 ms, Memory: 161608 KB -BaekjoonHub

--- 백준/Silver/29791. 에르다 노바와 오리진 스킬/에르다 노바와 오리진 스킬.py
+++ 백준/Silver/29791. 에르다 노바와 오리진 스킬/에르다 노바와 오리진 스킬.py
@@ -1,26 +1,19 @@
-def count_kills(n, m, d, s):
-    s.sort()
-    d.sort()
-    E_before = d[0]
-    O_before = s[0]
-    E_kills = 1
-    O_kills = 1
+def count_kills(count, time):
+    A = list(map(int, input().split()))
+    A.sort()
+    resist_before = A[0]
+    resist = 1
 
-    for i in range(1, n):
-        if d[i]-E_before >= 100:
-            E_kills += 1
-            E_before = d[i]
-    for j in range(1, m):
-        if s[j]-O_before >= 360:
-            O_kills += 1
-            O_before = s[j]
+    for i in range(1, count):
+        if A[i]-resist_before >= time:
+            resist += 1
+            resist_before = A[i]
 
-    return [E_kills, O_kills]
+    return resist
 
 
 n, m = map(int, input().split())
-d = list(map(int, input().split()))
-s = list(map(int, input().split()))
 
-result = count_kills(n, m, d, s)
-print(*result)
+E = count_kills(n, 100)
+O = count_kills(m, 360)
+print(E, O)
