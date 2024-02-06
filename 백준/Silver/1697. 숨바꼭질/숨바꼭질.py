3f0a21090c4bf6379b3f0a1e339e9201c930e58d
sknyuki <96415820+sknyuki@users.noreply.github.com>
2024-02-06 14:16:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,25 +1,20 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
+input=sys.stdin.readline
 
 
-# X-1 X+1 2*X로 너비탐색
-
-
-def bfs(N, K):
-    que = deque()
+def bfs(N,K):
+    que=deque()
     que.append(N)
     while que:
-        a = que.popleft()
-        if a == K:
-            print(time[a])
-            break
-        for i in (a-1, a+1, a*2):
-            if 0 <= i <= 100000 and time[i] == 0:
-                time[i] = time[a]+1
+        a=que.popleft()
+        if a==K:
+            return Time[a]
+        for i in (a+1,a-1,2*a):
+            if 0<=i<=100000 and Time[i]==0:
+                Time[i]=Time[a]+1
                 que.append(i)
 
-
-N, K = map(int, input().split())
-time = [0]*(100001)
-bfs(N, K)
+N,K=map(int,input().split())
+Time=[0]*100001
+print(bfs(N,K))
